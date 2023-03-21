2e4f94c96339336b7e62ed30050103c914323672
David Kruger <me@krugerdavid.com>
2023-03-21 13:45:17 -0300

    Fixed generate qr function and added revert hook alias functionality

--- src/BancardQR.php
+++ src/BancardQR.php
@@ -4,26 +4,25 @@ namespace KrugerDavid\LaravelBancardQR;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7\Response;
 use Illuminate\Support\Facades\Log;
 
 class BancardQR
 {
-    private static function generateToken()
-    {
-        $token = base64_encode("apps/" . config('bancard-qr.public_key') . ":" . config('bancard-qr.private_key'));
-        return "Basic {$token}";
-    }
-
+    /**
+     * 
+     */
     public static function generate_qr(int $amount, ?string $description = null, ?array $promotions = null)
     {
         $headers = [
-            'Authorization: ' . self::generateToken(),
-            'Content-Type: application/json'
+            'Authorization' => self::generateToken(),
+            'Content-Type' => 'application/json'
         ];
-        Log::debug('Response', ['headers' => $headers]);
 
-        try {
-            $url = "commerces/". config("bancard-qr.commerce.code") . "/branches/" . config("bancard-qr.commerce.branch") . "/selling/generate-qr-express";
+        try 
+        {
+            $url = 'commerces/%s/branches/%s/selling/generate-qr-express';
+            $url = sprintf($url, config("bancard-qr.commerce.code"), config("bancard-qr.commerce.branch"));
 
             $client = new Client([
                 'base_uri' => config('bancard-qr.service_url')
@@ -42,19 +41,57 @@ class BancardQR
             
 
             $res = $client->request('POST',$url, [
-                'json' => $json,
-                'headers' => $headers
+                'headers' => $headers,
+                'body' => json_encode($json)
             ]);
 
-
-            return response()->json([
-                'code' => $res->getStatusCode()
-            ]);
+            return response()->json(self::responseFromBancard($res));
 
         } catch (GuzzleException $e) {
             return response()->json([
                 'error' => $e->getMessage()
             ], 500);
         }
+    }
+
+    public static function revert(string $hook_alias)
+    {
+        try 
+        {
+            $url = 'commerces/%s/branches/%s/selling/payments/revert/%s';
+            $url = sprintf($url, config("bancard-qr.commerce.code"), config("bancard-qr.commerce.branch"), $hook_alias);
+
+            $headers = [
+                'Authorization' => self::generateToken(),
+                'Content-Type' => 'application/json'
+            ];
+
+            $client = new Client([
+                'base_uri' => config('bancard-qr.service_url')
+            ]);
+
+            $res = $client->request('PUT', $url, [
+                'headers' => $headers
+            ]);
+
+            return response()->json(self::responseFromBancard($res));
+
+        } catch (GuzzleException $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    private static function generateToken()
+    {
+        $token = base64_encode(sprintf('apps/%s:%s', config('bancard-qr.public_key'), config('bancard-qr.private_key')));
+        return "Basic {$token}";
+    }
+
+    private static function responseFromBancard(Response $response)
+    {
+        $res = @json_decode((string)$response->getBody()->getContents());
+        return $res;
     }
 }
